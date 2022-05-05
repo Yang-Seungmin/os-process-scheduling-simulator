623358ef1ab0d9dfd0f973b95bc406ccb41e2681
leeahwi <48510004+leeahwi@users.noreply.github.com>
2022-05-06 02:59:23 +0900

    SPN 버그 수정 및 주석 추가

--- src/main/kotlin/schedulingalgorithm/SPN.kt
+++ src/main/kotlin/schedulingalgorithm/SPN.kt
@@ -6,6 +6,13 @@ class SPN : SchedulingAlgorithm(
     algorithmName = "SPN",
     readyQueueSize = 1
 ) {
+
+
+    /**
+     * singleReadyQueue에서 가장 작은 workload를 가진 process를 singleReadyQueue에서 꺼내어 반환한다.
+     *
+     * @return process
+     */
     private fun pollShortestProcess(): Process? {
         val process = singleReadyQueue.minByOrNull { it.workload }
         singleReadyQueue.remove(process)
@@ -16,15 +23,27 @@ class SPN : SchedulingAlgorithm(
         // Do nothing
     }
 
+
+    /**
+     * processes에서 singleReadyQueue에 process들을 순서대로 삽입한다.
+     * SchedulingAlgorithm class의 abstract func
+     *
+     * @param processes arrival time과 현재 time이 일치하는 프로세스 리스트
+     */
     override fun putProcessIntoReadyQueue(processes: List<Process>) {
         processes.forEach {
             singleReadyQueue.offer(it)
         }
     }
 
+    /**
+     * singleReadyQueue에 process가 있는 경우, 빈 core에 singleReadyQueue에서 가장 짧은 process를 꺼내서 전달
+     * SchedulingAlgorithm class의 abstract func
+     *
+     */
     override fun beforeWork(time: Int) {
         cores.forEachIndexed { i, core ->
-            if (core.process == null && readyQueue.isNotEmpty())
+            if (core.process == null && singleReadyQueue.isNotEmpty())
                 core.process = pollShortestProcess()
         }
     }
