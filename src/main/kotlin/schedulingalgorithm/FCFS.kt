b116312ef767d0bc31deaf9e0672b4daac83c587
leeahwi <48510004+leeahwi@users.noreply.github.com>
2022-05-06 02:12:20 +0900

    FCFS 주석 수정

--- src/main/kotlin/schedulingalgorithm/FCFS.kt
+++ src/main/kotlin/schedulingalgorithm/FCFS.kt
@@ -26,7 +26,6 @@ class FCFS : SchedulingAlgorithm(
      * singleReadyQueue에 process가 있는 경우, 빈 core에 singleReadyQueue에 맨 앞에 있는 process를 꺼내서 전달
      * SchedulingAlgorithm class의 abstract func
      *
-     * @param processes arrival time과 현재 time이 일치하는 프로세스 리스트
      */
     override fun beforeWork(time: Int) {
         cores.forEachIndexed { i, core ->
